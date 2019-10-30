1a8753aa96506dfea04ea675e3e4497be4cc505e
canhetingsky <2380348167@qq.com>
2019-10-30 21:07:49 +0800

    fix: 🐛 Modify solution for NO.32
    Resolve timeout issues in some cases
    
    Closes: #5

--- Python3/32.longest-valid-parentheses.py
+++ Python3/32.longest-valid-parentheses.py
@@ -11,32 +11,33 @@ class Solution:
     def longestValidParentheses(self, s: str) -> int:
         max_length = 0
         left_count = right_count = 0
-        right_str = s.count(')')
-        for i in range(len(s)):
-            if i + max_length > len(s):
-                break
-            left_count = right_count = 0
-            count, j = 0, i
-            while j < len(s):
-                if s[j] == '(':
-                    left_count += 1
-                elif s[j] == ')':
-                    right_count += 1
+        for i in range(len(s)):  # left to right scan
+            if s[i] == '(':
+                left_count += 1
+            elif s[i] == ')':
+                right_count += 1
 
-                if left_count == right_count:
-                    count = left_count * 2
-                elif left_count < right_count:
-                    break
+            if left_count == right_count:
+                max_length = max(max_length, right_count * 2)
+            elif left_count < right_count:
+                left_count = right_count = 0
 
-                if (left_count - right_count) > (len(s) - j):
-                    break
-                if left_count > right_str:
-                    break
-                j += 1
-            max_length = max(max_length, count)
+        left_count = right_count = 0
+        for i in range(len(s))[::-1]:  # right to left scan
+            if s[i] == '(':
+                left_count += 1
+            elif s[i] == ')':
+                right_count += 1
+
+            if left_count == right_count:
+                max_length = max(max_length, left_count * 2)
+            elif left_count > right_count:
+                left_count = right_count = 0
 
         return max_length
 # @lc code=end
 
-# Time Limit Exceeded
-# 228/230 cases passed(N/A)
+# Accepted
+# 230/230 cases passed(48 ms)
+# Your runtime beats 90.94 % of python3 submissions
+# Your memory usage beats 44.44 % of python3 submissions(13.8 MB)
