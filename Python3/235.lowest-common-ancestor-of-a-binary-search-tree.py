a5ad85306ba2b247ac78a164aa60a215c6974eec
canhetingsky <2380348167@qq.com>
2019-11-16 21:56:46 +0800

    feat: 🎸 Add solution for NO.234,235

---
+++ Python3/235.lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=235 lang=python3
+#
+# [235] Lowest Common Ancestor of a Binary Search Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or not p or not q:
+            return None
+
+        if (max(p.val, q.val) < root.val):
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif (min(p.val, q.val) > root.val):
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
+# @lc code=end
+
+# Accepted
+# 27/27 cases passed(68 ms)
+# Your runtime beats 99.85 % of python3 submissions
+# Your memory usage beats 100 % of python3 submissions(16.6 MB)
